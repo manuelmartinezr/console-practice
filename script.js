388cd727fa5a0a4462e4b5634d8487d21da5a16c
Manuel Martínez <manuelmartinezr18@gmail.com>
2024-08-02 15:56:55 -0500

    noo

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-import { getComputerChoice, getHumanChoice } from "./functions";
+function getComputerChoice(choices){
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+};
+function getHumanChoice(choices){
+    let choice = null;
+    while (!choices.includes(choice)){
+        choice = window.prompt("what's your choice hon")
+        if (!choices.includes(choice)) {
+            alert(`"${choice}" is not a valid choice. Please try again.`);
+        }
+    }
+    return choice
+}
 
 let choices = ["rock", "paper", "scissors"];
 
